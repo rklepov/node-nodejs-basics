367cb3706b2e1643b7712b1ac093c1dbd7a68caf
Roman Klepov <me@rklepov.ru>
2024-10-04 23:40:59 +0300

    feat: implement cli/env.js task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+  const env = process.env;
+  Object.keys(env).forEach((key) => {
+    if (key.match(/^RSS_./)) console.log(`${key}=${env[key]}`);
+  });
 };
 
 parseEnv();
