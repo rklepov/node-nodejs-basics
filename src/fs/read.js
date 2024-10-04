a685cd8835dea885b463bbb0927c1c3aade5916c
Roman Klepov <me@rklepov.ru>
2024-10-04 23:22:43 +0300

    feat: implement fs/read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const cwd = import.meta.dirname;
+  const source = path.join(cwd, 'files', 'fileToRead.txt');
+
+  const input = fs.createReadStream(source, { flags: 'r' });
+  input
+    .on('error', (e) => {
+      throw new Error(`FS operation failed: ${e.message}`);
+    })
+    .on('close', () => {
+      process.stdout.write(os.EOL);
+    })
+    .pipe(process.stdout);
 };
 
 await read();
