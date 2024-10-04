73d6b3a143547fe4f464502142636264ac942f2d
Roman Klepov <me@rklepov.ru>
2024-10-04 22:06:50 +0300

    feat: implement fs/rename.js task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,41 @@
+import fsp from 'fs/promises';
+import path from 'path';
+
+async function checkFileExists(path) {
+  try {
+    await fsp.access(path);
+    return Promise.resolve(true);
+  } catch {
+    return Promise.resolve(false);
+  }
+}
+
 const rename = async () => {
-    // Write your code here 
+  const cwd = import.meta.dirname;
+  const source = path.join(cwd, 'files', 'wrongFilename.txt');
+  const target = path.join(cwd, 'files', 'properFilename.md');
+
+  try {
+    console.log(
+      `${path.relative(cwd, source)} -> ${path.relative(cwd, target)}`,
+    );
+
+    if (!(await checkFileExists(source))) {
+      throw new Error(
+        `the source file doesn't exist: ${path.relative(cwd, source)}`,
+      );
+    }
+
+    if (await checkFileExists(target)) {
+      throw new Error(
+        `the target file already exists: ${path.relative(cwd, target)}`,
+      );
+    }
+
+    await fsp.rename(source, target);
+  } catch (e) {
+    throw new Error(`FS operation failed: ${e.message}`);
+  }
 };
 
 await rename();
