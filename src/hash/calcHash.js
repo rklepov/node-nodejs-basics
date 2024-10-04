9ae1ee56ab2de0157e0c6c25e7cbded4780286d6
Roman Klepov <me@rklepov.ru>
2024-10-04 23:43:41 +0300

    feat: implement hash/calcHash.js task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const cwd = import.meta.dirname;
+  const hash = crypto.createHash('sha256');
+  const input = fs.createReadStream(
+    path.join(cwd, 'files', 'fileToCalculateHashFor.txt'),
+  );
+
+  input
+    .on('close', () => {
+      process.stdout.write(os.EOL);
+    })
+    .pipe(hash)
+    .setEncoding('hex')
+    .pipe(process.stdout);
 };
 
 await calculateHash();
