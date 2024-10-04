752ef8648c76c384fb6f7cc68566e20ca009ef39
Roman Klepov <me@rklepov.ru>
2024-10-05 00:25:10 +0300

    feat: implement cp/cp.js task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import * as cp from 'child_process';
+import * as path from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const cwd = import.meta.dirname;
+  const child = cp.fork(path.join(cwd, 'files', 'script.js'), args, {
+    stdio: 'inherit',
+  });
+
+  child.on('exit', (code) => {
+    console.log(`child process exited with code ${code}`);
+    console.log('BYE!');
+    process.exit(code);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['--arg1', 'value1', 'positional_arg1', 'positional_arg2']);
