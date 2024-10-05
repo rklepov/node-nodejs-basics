d993a971199b8856c2dfd6525be0f5d9bc052dd1
Roman Klepov <me@rklepov.ru>
2024-10-05 06:09:15 +0300

    feat: implement wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import os from 'os';
+import path from 'path';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+  const nproc = os.cpus().length;
+  const cwd = import.meta.dirname;
+
+  const result = await Promise.all(
+    [...Array(nproc).keys()].map((n) => {
+      return new Promise((resolve) => {
+        new Worker(path.join(cwd, 'worker.js'), {
+          workerData: n + 10,
+        })
+          .on('message', (msg) => {
+            resolve({ status: 'resolved', data: msg.result });
+          })
+          .on('error', () => {
+            resolve({ status: 'error', data: null });
+          });
+      });
+    }),
+  );
+
+  console.log(result);
 };
 
 await performCalculations();
